c017f78a2882d6489e7a1b634e3dccb464c99ad5
SmallDumplings <katyachecryglina@gmail.com>
2024-12-07 02:06:54 +0500

    third task

--- main.cpp
+++ main.cpp
@@ -38,9 +38,37 @@ void displayHeader(ntfc_header* header) {
     cout << "Number of file recording: 0x" << hex << int(header->number_file_recording) << endl;
 }
 
+struct ntfc_header_atribut
+{
+    uint32_t type; //тип атрибута
+    uint32_t lenght; // длина атрибута
+    uint8_t flag; // флаг нерезидентного атрибута
+    uint8_t lenght_name; // длина имени
+    uint16_t offset_name; // смещение имени
+    uint16_t flag_pack; // флаг упаковки
+    uint16_t indificator; // индификатор
+    uint32_t size_in_byte; // размер содержимого в байтах
+    uint16_t offset_content; // смещение содержимого атрибута
+    uint8_t flag_indexing; // флаг индексирования
+    uint8_t context; //заполнение
+};
+
+string what_is_struct(uint32_t type){
+    if (type == 0x10) return "$STANDART_INFORMATION";
+    else if (type == 0x20) return "$ATTRIBUTE_LIST";
+    else if (type == 0x30) return "$FILE_NAME";
+    else if (type == 0x40) return "$OBJECT_ID";
+    else if (type == 0x50) return "$SECURITY_DESCRIPTOR";
+    else if (type == 0x80) return "$DATA";
+    else if (type == 0x90) return "$INDEX_ROOT";
+    else if (type == 0xA0) return "$INDEX_ALLOCATION";
+    else if (type == 0xB0) return "$BITMAP";
+    else if (type == 0x100) return "$LOGGED_UTILITY_STREAM";
+
+}
+
 // Восстановить выравнивание
 #pragma pack(pop)
-uint16_t offset_f_atribute;
 int main()
 {
     // Открыть файл в двоичном режиме
@@ -67,7 +95,21 @@ int main()
         // Вывести жирные поля структуры-2 пункт
         displayHeader(n_header);
 
+
+        // 3 пункт Вывод названия и типа атрибутов
+        uint16_t offset_f_atribute;
+        offset_f_atribute = int(n_header->offset_atribute);
+        ntfc_header_atribut* n_atribut = reinterpret_cast<ntfc_header_atribut*>(&ntfc_data[offset_f_atribute]);
+
+        while (n_atribut->type != 0xFFFFFFFF){
+
+            cout << endl << "Name of atribut:" << what_is_struct(n_atribut->type) << endl;
+            cout << "Type of atribut: 0x" << hex << n_atribut->type << endl;
+            offset_f_atribute += (n_atribut->lenght);
+            n_atribut = reinterpret_cast<ntfc_header_atribut*>(&ntfc_data[offset_f_atribute]);
         }
+
+    }
 
     return 0;
 }
